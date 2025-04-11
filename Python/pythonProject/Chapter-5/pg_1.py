0eb9063c588457d9ca83ef5d65079c0194f790f3
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-04-11 17:58:09 +0600

    Python Chapter 5 tuple done

---
+++ Python/pythonProject/Chapter-5/pg_1.py
@@ -0,0 +1,12 @@
+my_tuple = (12, 6, -8, "Bd", True)
+print(my_tuple)
+print(my_tuple[3])
+print(my_tuple[-3])
+print(my_tuple[1:])
+print(my_tuple[::2])
+print(len(my_tuple))
+
+my_tuple1 = (1, 2, (4, 5), 6)
+print(my_tuple1)
+
+print(my_tuple + my_tuple1)
---
+++ Python/pythonProject/Chapter-5/pg_2.py
@@ -0,0 +1,13 @@
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+my_list = [1, 2, 3, 4]
+print(my_list)
+print(type(my_list))
+
+my_tuple = tuple(my_list)
+print(my_tuple)
+print(type(my_tuple))
