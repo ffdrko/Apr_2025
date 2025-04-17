9f576f4b9673ab4e0b89c36ce70d00c4478edf3c
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-04-17 12:45:25 +0600

    Python Chapter 9 dict method-3 done

---
+++ Python/pythonProject/Chapter-9/pg_7.py
@@ -0,0 +1,10 @@
+employee = {
+    "id": 101,
+    "name": "Fahim",
+    "age": 25,
+    "dept": "Senior dev",
+    "salary": 10000
+}
+
+for i in employee.keys():
+    print(i)
---
+++ Python/pythonProject/Chapter-9/pg_9.py
@@ -0,0 +1,10 @@
+employee = {
+    "id": 101,
+    "name": "Fahim",
+    "age": 25,
+    "dept": "Senior dev",
+    "salary": 10000
+}
+
+for i, j in employee.items():
+    print(i, j)
