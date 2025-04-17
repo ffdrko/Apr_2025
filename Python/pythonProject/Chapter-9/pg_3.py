b373b9ddd4e425ee284a5de13a5b5a52272280b3
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2025-04-17 12:16:05 +0600

    Python Chapter 9 dict method done

---
+++ Python/pythonProject/Chapter-9/pg_3.py
@@ -0,0 +1,16 @@
+employee = {
+    "id": 101,
+    "name": "Fahim",
+    "age": 25,
+    "dept": "Senior dev",
+    "salary": 10000
+}
+
+em_id = employee.get('id')
+print(em_id)
+
+print(employee.keys())
+print(employee.values())
+
+em_item = employee.items()
+print(em_item)
